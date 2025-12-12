0f266a17a82ce9c9d8cd593e7468d09b8a15663f
jahid <jahidacs249sept.gmail.com>
2025-12-12 22:11:43 +0530

    Updated code 12 Dec

---
+++ src/main/java/five/ExceptionDemo.java
@@ -0,0 +1,21 @@
+package five;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        System.out.println(a); // 10
+
+        int result = a / 0;
+        System.out.println(result);
+
+        //Exception in thread "main" java.lang.ArithmeticException: / by zero
+//
+//        try{
+//
+//        }
+//        catch (Exception e){
+//
+//        }
+    }
+}
