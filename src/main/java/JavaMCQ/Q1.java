88a1f28623a903b54d5365a73f0a57f99190205b
jahid <jahidacs249sept.gmail.com>
2025-12-28 11:37:23 +0530

    Java MCQ

---
+++ src/main/java/JavaMCQ/Q1.java
@@ -0,0 +1,11 @@
+package JavaMCQ;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int x = 0;
+//        if (x = 1) {
+//            System.out.println("True");
+//        }
+    }
+}
+// Compilation error
---
+++ src/main/java/JavaMCQ/Q2.java
@@ -0,0 +1,10 @@
+package JavaMCQ;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i < 3; i++);
+        System.out.println(i);
+    }
+}
+//3
