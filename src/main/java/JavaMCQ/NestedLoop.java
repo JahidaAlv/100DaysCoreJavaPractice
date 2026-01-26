273a53d60599b067ca3a7a7f978448fdd201b9e6
jahid <jahidacs249sept.gmail.com>
2026-01-26 16:10:22 +0530

    updated code

--- src/main/java/JavaMCQ/NestedLoop.java
+++ src/main/java/JavaMCQ/NestedLoop.java
@@ -3,16 +3,16 @@ package JavaMCQ;
 // Loop inside loop is called as nested loop.
 
 
-public class NestedLoop {
-    public static void main(String[] args) {
-        for (int i =1; i <=5; i++){
-            for (int j = 1; j <= i; j++){
-                System.out.print("*");  // same line
-            }
-            System.out.println(); // next line
-        }
-    }
-}
+//public class NestedLoop {
+//    public static void main(String[] args) {
+//        for (int i =1; i <=5; i++){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print("*");  // same line
+//            }
+//            System.out.println(); // next line
+//        }
+//    }
+//}
 
 
 //public class NestedLoop {
@@ -44,4 +44,19 @@ public class NestedLoop {
 //        1234
 //        12345
 
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        for (int i =1; i <=5; i++){
+            for (int k = 5-i; k >= 1; k--){
+                System.out.print(" ");
+            }
+           for (int j = 1; j <=i; j++){
+               System.out.print
+                       ("* ");
+           }
+            System.out.println();
+        }
+    }
+}
 
