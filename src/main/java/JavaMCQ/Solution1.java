9118360a65d1abbd5054bace41aa7aefa44885c0
jahid <jahidacs249sept.gmail.com>
2026-02-07 20:06:56 +0530

    String Question

---
+++ src/main/java/JavaMCQ/Solution1.java
@@ -0,0 +1,9 @@
+package JavaMCQ;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        String s = "100";
+        int number = Integer.parseInt(s);
+        System.out.println(number);
+    }
+}
---
+++ src/main/java/JavaMCQ/Test.java
@@ -0,0 +1,9 @@
+package JavaMCQ;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "Hello";
+        s.toUpperCase();
+        System.out.println(s);
+    }
+}
